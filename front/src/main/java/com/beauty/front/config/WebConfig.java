b7fe3b987c45db521fef17f8014e4bf91da86c0b
Nastya B <2murka2@gmail.com>
2025-06-03 18:55:15 +0300

    fix front service

--- front/src/main/java/com/beauty/front/config/WebConfig.java
+++ front/src/main/java/com/beauty/front/config/WebConfig.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//        registry.addResourceHandler("/static/**")
-//                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:uploads/images/");
     }
